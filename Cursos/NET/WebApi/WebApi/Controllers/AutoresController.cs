9adc103288d0be24e2b02405b35faf9babdbb8e2
Brian Ponce <brian.ponce@vensunrise.site>
2019-10-09 16:35:37 -0300

    Initial

---
+++ Cursos/NET/WebApi/WebApi/Controllers/AutoresController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Context;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AutoresController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Autor>>> Get()
+        {
+            return await _db.Autores.ToListAsync();
+        }
+
+        [HttpGet("id", Name = "ObtenerAutor")]
+        public async Task<ActionResult<Autor>> Get(int id)
+        {
+            Autor autor = await _db.Autores.FirstOrDefaultAsync(x => x.Id == id);
+            if (autor == null)
+                return NotFound();
+
+            return autor;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Autor autor)
+        {
+            _db.Add(autor);
+            await _db.SaveChangesAsync();
+            return new CreatedAtRouteResult("ObtenerAutor", new { id = autor.Id }, autor);
+        }
+
+        [HttpPut("id")]
+        public async Task<ActionResult> Put([FromBody] Autor autor, int id)
+        {
+            if (id != autor.Id)
+                return BadRequest();
+
+            _db.Entry(autor).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        public async Task<ActionResult<Autor>> Delete(int id)
+        {
+            Autor autor = await _db.Autores.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (autor == null)
+                return NotFound();
+
+            _db.Autores.Remove(autor);
+            _db.SaveChanges();
+            return autor;
+        }
+    }
+}
---
+++ Cursos/NET/WebApi/WebApi/Controllers/LibrosController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Context;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibrosController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LibrosController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Libro>>> Get()
+        {
+            return await _db.Libros.ToListAsync();
+        }
+
+        [HttpGet("id", Name = "ObtenerLibro")]
+        public async Task<ActionResult<Libro>> Get(int id)
+        {
+            Libro libro = await _db.Libros.FirstOrDefaultAsync(x => x.Id == id);
+            if (libro == null)
+                return NotFound();
+
+            return libro;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Libro libro)
+        {
+            _db.Add(libro);
+            await _db.SaveChangesAsync();
+            return new CreatedAtRouteResult("ObtenerLibro", new { id = libro.Id }, libro);
+        }
+
+        [HttpPut("id")]
+        public async Task<ActionResult> Put([FromBody] Libro libro, int id)
+        {
+            if (libro.Id != id)
+                return BadRequest();
+
+            _db.Entry(libro).State = EntityState.Modified;
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("id")]
+        public async Task<ActionResult<Libro>> Delete(int id)
+        {
+            Libro libro = await _db.Libros.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (libro == null)
+                return NotFound();
+
+            _db.Libros.Remove(libro);
+            await _db.SaveChangesAsync();
+            return libro;
+        }
+    }
+}
